81cf0297b0240f9de8b2c3cc84c0723c0c4d0c29
Ian Kane <ianwilliamkane@gmail.com>
2022-01-20 17:08:19 +0000

    Add fucntions to diplay hangman image

--- run.py
+++ run.py
@@ -4,6 +4,40 @@ import random
 def clear():
     os.system("cls")
 
+def print_hangman(vaulues):
+    print()
+    print("\t +--------+")
+    print("\t |       | |")
+    print("\t {}       | |".format(values[0]))
+    print("\t{}{}{}      | |".format(values[1], values[2], values[3]))
+    print("\t {}       | |".format(values[4]))
+    print("\t{} {}      | |".format(values[5],values[6]))
+    print("\t         | |")
+    print("  _______________|_|___")
+    print("  `````````````````````")
+    print()  
+
+def hangman_game(word):
+    clear()
+    word_display = []
+    correct_letters = []
+    incorrect = []
+    chances = 0
+    hangman_values = ['O', '/', '|', '\\', '|', '/', '\\']
+    show_hangman_values = [' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    for char in word:
+        if char.isalpha():
+            word_display.append('_')
+            correct_letters.append(char.upper())
+        else:
+            word_display.append(char)
+
+    while True:
+        clear()
+        print_hangman(show_hangman_values)
+        print_word(word_display)
+        print()    
+
 if __name__ == "_main_":
     clear()
 

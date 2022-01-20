7d9883818ab131d1ada15df7edb3b5b621df59a3
Ian Kane <ianwilliamkane@gmail.com>
2022-01-20 16:15:48 +0000

    Add beginning of hangman game with three question categories

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import os
+
+def clear():
+    os.system("cls")
+
+if __name__ == "_main_":
+    clear()
+
+    topics = {1: "Football players", 2: "Rockstars", 3: "Marvel Characters"}
+
+    dataset = { "Football players":["MO SALAH", "LIONEL MESSI", "KEVIN DE BRUYNE", "CRISTIANO RONALDO", "NGOLO KANTE",
+    "KYLIAN MBAPPE", "NEYMAR", "KARIM BENZEMA" "DANI ALVES", "ZLATAN IBRAHIMOVIC"],\
+    "Rockstars": ["LIAM GALLAGHER", "AXEL ROSE", "ERIC CLAPTON", "SLASH", "PAUL MCCARTNEY", "MICK JAGGER", "OZZY OSBOURNE",
+    "JIMI HENDRIX", "BRUCE SPRINGSTEEN", "JOHN LENNON", "DAVID BOWIE"],
+     "Marvel Characters":["CAPTAIN AMERICA", "IRON MAN", "THANOS", "HAWKEYE", "BLACK PANTHER", "BLACK WIDOW", "SPIDERMAN",
+     "STARLORD", "THOR", "DOCTOR STRANGE"]
+     }
+
+    

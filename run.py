dc779864e7ae765a9c971406fd7214cf24215429
Ian Kane <ianwilliamkane@gmail.com>
2022-01-22 18:17:04 +0000

    Run code through Pep8 and fix errors

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import random
 import os
 
+
 def clear():
     os.system("cls")
 
@@ -12,17 +13,17 @@ def print_hangman(values):
     print("\t {}       | |".format(values[0]))
     print("\t{}{}{}      | |".format(values[1], values[2], values[3]))
     print("\t {}       | |".format(values[4]))
-    print("\t{} {}      | |".format(values[5],values[6]))
+    print("\t{} {}      | |".format(values[5], values[6]))
     print("\t         | |")
     print("  _______________|_|___")
     print("  `````````````````````")
     print()
 
+
 def print_hangman_win():
     print()
     print("\t +--------+")
     print("\t         | |")
- 
     print("\t         | |")
     print("\t O       | |")
     print("\t/|\\      | |")
@@ -31,7 +32,9 @@ def print_hangman_win():
     print("  `````````````````````")
     print()
 
+
 word_display = []
+
 
 def print_word(values):
     print()
@@ -40,11 +43,13 @@ def print_word(values):
         print(x, end="")
     print()
 
+
 def check_win(values):
     for char in values:
-        if char == '_':
+        if char == "_":
             return False
     return True
+
 
 def hangman_game(word):
     clear()
@@ -52,11 +57,11 @@ def hangman_game(word):
     correct_letters = []
     incorrect = []
     chances = 0
-    hangman_values = ['O', '/', '|', '\\', '|', '/', '\\']
-    show_hangman_values = [' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    hangman_values = ["O", "/", "|", "\\", "|", "/", "\\"]
+    show_hangman_values = [" ", " ", " ", " ", " ", " ", " "]
     for char in word:
         if char.isalpha():
-            word_display.append('_')
+            word_display.append("_")
             correct_letters.append(char.upper())
         else:
             word_display.append(char)
@@ -103,21 +108,51 @@ def hangman_game(word):
                 print_hangman_win()
                 print("The word is :", word.upper())
                 break
-        
-            
+
 
 if __name__ == "__main__":
     clear()
 
     topics = {1: "Football players", 2: "Rockstars", 3: "Marvel Characters"}
 
-    dataset = { "Football players":["MO SALAH", "LIONEL MESSI", "KEVIN DE BRUYNE", "CRISTIANO RONALDO", "NGOLO KANTE",
-    "KYLIAN MBAPPE", "NEYMAR", "KARIM BENZEMA" "DANI ALVES", "ZLATAN IBRAHIMOVIC"],\
-    "Rockstars": ["LIAM GALLAGHER", "AXEL ROSE", "ERIC CLAPTON", "SLASH", "PAUL MCCARTNEY", "MICK JAGGER", "OZZY OSBOURNE",
-    "JIMI HENDRIX", "BRUCE SPRINGSTEEN", "JOHN LENNON", "DAVID BOWIE"],
-     "Marvel Characters":["CAPTAIN AMERICA", "IRON MAN", "THANOS", "HAWKEYE", "BLACK PANTHER", "BLACK WIDOW", "SPIDERMAN",
-     "STARLORD", "THOR", "DOCTOR STRANGE"]
-     }
+    dataset = {
+        "Football players": [
+            "MO SALAH",
+            "LIONEL MESSI",
+            "KEVIN DE BRUYNE",
+            "CRISTIANO RONALDO",
+            "NGOLO KANTE",
+            "KYLIAN MBAPPE",
+            "NEYMAR",
+            "KARIM BENZEMA" "DANI ALVES",
+            "ZLATAN IBRAHIMOVIC",
+        ],
+        "Rockstars": [
+            "LIAM GALLAGHER",
+            "AXEL ROSE",
+            "ERIC CLAPTON",
+            "SLASH",
+            "PAUL MCCARTNEY",
+            "MICK JAGGER",
+            "OZZY OSBOURNE",
+            "JIMI HENDRIX",
+            "BRUCE SPRINGSTEEN",
+            "JOHN LENNON",
+            "DAVID BOWIE",
+        ],
+        "Marvel Characters": [
+            "CAPTAIN AMERICA",
+            "IRON MAN",
+            "THANOS",
+            "HAWKEYE",
+            "BLACK PANTHER",
+            "BLACK WIDOW",
+            "SPIDERMAN",
+            "STARLORD",
+            "THOR",
+            "DOCTOR STRANGE",
+        ],
+    }
 
     while True:
         print()
@@ -135,16 +170,15 @@ if __name__ == "__main__":
             print("Wrong choice!! Try again")
             continue
 
-        if choice > len(topics)+1:
+        if choice > len(topics) + 1:
             clear()
             print("No such topic!! Try again.")
             continue
-        elif choice == len(topics)+1:
+        elif choice == len(topics) + 1:
             print()
             print("Thank you for playing!")
             break
-        
+
         chosen_topic = topics[choice]
         ran = random.choice(dataset[chosen_topic])
         hangman_game(ran)
-    
